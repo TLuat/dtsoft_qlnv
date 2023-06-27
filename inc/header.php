fe4a4999d47ab62ed198aaf133eb3fcef163f2ff
TLuat <93259832+TLuat@users.noreply.github.com>
2023-06-27 17:50:32 +0700

    Thêm hiện tên vai trò, bộ phận, khu vực vào header của người dùng đã đăng nhập

--- inc/header.php
+++ inc/header.php
@@ -188,16 +188,42 @@
         <div class="px-3 py-3 lg:px-5 lg:pl-3">
           <div class="flex items-center justify-between">
             <div class="flex items-center justify-start">
-              <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-700 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-700 dark:hover:bg-gray-200 dark:focus:ring-gray-600">
-                  <span class="sr-only">Open sidebar</span>
-                  <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                     <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-                  </svg>
-               </button>
               <a href="index.php" class="flex ml-2 md:mr-24">
                 <img src="assets/img/logo.jpg" class="h-12 mr-3" alt="FlowBite Logo" />
               </a>
             </div>
+	    <div class="flex items-center justify-start relative inline-flex items-center p-0.5 mb-2 mr-2 text-sm font-medium rounded-lg border border-blue-900">
+              <span class="relative px-5 py-2.5 bg-white rounded-md border-r border-blue-900">
+                <i class="fa-regular fa-user"></i>
+                  <?php 
+                    $sql = "SELECT tenvaitro from vaitro where id_vaitro = '" . $_SESSION['id_vaitro'] ."'";
+                    $result = mysqli_query($connect, $sql);
+                    $row = mysqli_fetch_array($result);
+                    echo $row['tenvaitro'];
+                  ?>
+              </span>
+              <span class="relative px-5 py-2.5 bg-white rounded-md border-r border-blue-900">
+              <i class="fa-solid fa-cheese"></i>
+                  <?php 
+                    if(!empty($_SESSION['id_bophan'])){
+                      $sql = "SELECT tenbophan from bophan where id_bophan = '" . $_SESSION['id_bophan'] ."'";
+                      $result = mysqli_query($connect, $sql);
+                      $row = mysqli_fetch_array($result);
+                      echo $row['tenbophan'];
+                    } echo "Bộ phận";
+                    
+                  ?>
+              </span>
+              <span class="relative px-5 py-2.5 bg-white rounded-md">
+                  <i class="fa-solid fa-globe"></i>
+                  <?php 
+                    $sql = "SELECT tenkhuvuc from khuvuc where id_khuvuc = '" . $_SESSION['id_khuvuc'] ."'";
+                    $result = mysqli_query($connect, $sql);
+                    $row = mysqli_fetch_array($result);
+                    echo $row['tenkhuvuc'];
+                  ?>
+              </span>
+            </div>	  
             <div class="flex items-center">
                 <div class="flex items-center ml-3">
                   <div class="flex">
